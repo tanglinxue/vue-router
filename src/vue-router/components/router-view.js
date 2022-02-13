ab82282f4976fa18490ae667f8a1ad285adc4c7a
tanglinxuework@163.com <tanglinxuework@163.com>
2022-02-13 23:06:58 +0800

    开发

--- src/vue-router/components/router-view.js
+++ src/vue-router/components/router-view.js
@@ -1,20 +1,26 @@
 export default {
   name: "router-view",
-  functional:true,//函数式组件,函数不用new,没有this,没有生命周期
-  render(h,{parent,data}) {
-    console.log(h)
+  functional: true,//函数式组件,函数不用new,没有this,没有生命周期
+  render(h, { parent, data }) {
     //this.$route有matched属性，这个属性有几个就依次的将他赋予到对应的router-view上
-
     //parent是当前父组件
     //data是这个组件上的一些标识
-    console.log(parent)
-    console.log(data)
     let route = parent.$route;
     let depth = 0;
+    data.routerView = true;//标识路由属性
+
+    console.log(parent)
+    while (parent) {
+      if (parent.$vnode && parent.$vnode.data.routerView) {
+        depth++
+      }
+      parent = parent.$parent
+    }
     let record = route.matched[depth]
-    if(!record){
+    console.log(route.matched)
+    if (!record) {
       return h()
     }
-    return h(record.component,data)
+    return h(record.component, data)
   }
 }
